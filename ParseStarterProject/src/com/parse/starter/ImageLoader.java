e44ca0de27b47d922658d331a7597be111d6226b
xDog <kielyk@live.ie>
2015-04-14 22:50:16 +0100

    Updated UI

--- ParseStarterProject/src/com/parse/starter/ImageLoader.java
+++ ParseStarterProject/src/com/parse/starter/ImageLoader.java
@@ -102,8 +102,7 @@ public class ImageLoader {
             int width_tmp = o.outWidth, height_tmp = o.outHeight;
             int scale = 1;
             while (true) {
-                if (width_tmp / 2 < REQUIRED_SIZE
-                        || height_tmp / 2 < REQUIRED_SIZE)
+                if (width_tmp / 2 < REQUIRED_SIZE || height_tmp / 2 < REQUIRED_SIZE)
                     break;
                 width_tmp /= 2;
                 height_tmp /= 2;
